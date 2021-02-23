b174c0eeeaf0f2e4589668f20e1af1be6309eb71
Leslie B Adams <leslethal93@gmail.com>
2021-02-22 21:07:18 -0600

    Winner functionality
    added a winner function

--- Base.js
+++ Base.js
@@ -37,71 +37,98 @@ var reset = document.getElementById('reset');
 function win() { 
   // top row
     if (tl_X.classList.contains('visible') && tm_X.classList.contains('visible') && tr_X.classList.contains('visible')) {
-      console.log('Top row X wins!')   
+      // console.log('Top row X wins!')
+      winScreen()   
     } 
     if (tl_O.classList.contains('visible') && tm_O.classList.contains('visible') && tr_O.classList.contains('visible')) {
-      console.log('Top row O wins!')   
+      // console.log('Top row O wins!') 
+      winScreen()  
     }
 
   // middle row
     if (ml_X.classList.contains('visible') && mm_X.classList.contains('visible') && mr_X.classList.contains('visible')) {
-      console.log('middle row X wins!')   
+      // console.log('middle row X wins!') 
+      winScreen()  
     } 
     if (ml_O.classList.contains('visible') && mm_O.classList.contains('visible') && mr_O.classList.contains('visible')) {
-      console.log('middle row O wins!')   
+      // console.log('middle row O wins!')  
+      winScreen() 
     }
 
   // bottom row
     if (bl_X.classList.contains('visible') && bm_X.classList.contains('visible') && br_X.classList.contains('visible')) {
-      console.log('bottom row X wins!')   
+      // console.log('bottom row X wins!')  
+      winScreen() 
     } 
     if (bl_O.classList.contains('visible') && bm_O.classList.contains('visible') && br_O.classList.contains('visible')) {
-      console.log('bottom row O wins!')   
+      // console.log('bottom row O wins!')   
+      winScreen()
     }
 
   // left column
     if (tl_X.classList.contains('visible') && ml_X.classList.contains('visible') && bl_X.classList.contains('visible')) {
-      console.log('left column X wins!')   
+      // console.log('left column X wins!')  
+      winScreen() 
     } 
     if (tl_O.classList.contains('visible') && ml_O.classList.contains('visible') && bl_O.classList.contains('visible')) {
-      console.log('left column O wins!')   
+      // console.log('left column O wins!') 
+      winScreen()  
     }
 
   // middle column
     if (tm_X.classList.contains('visible') && mm_X.classList.contains('visible') && bm_X.classList.contains('visible')) {
-      console.log('middle column X wins!')   
+      // console.log('middle column X wins!') 
+      winScreen()  
     } 
     if (tm_O.classList.contains('visible') && mm_O.classList.contains('visible') && bm_O.classList.contains('visible')) {
-      console.log('middle column O wins!')   
+      // console.log('middle column O wins!')  
+      winScreen() 
     }
 
   // right column
     if (tr_X.classList.contains('visible') && mr_X.classList.contains('visible') && br_X.classList.contains('visible')) {
-      console.log('right column X wins!')   
+      // console.log('right column X wins!')  
+      winScreen() 
     } 
     if (tr_O.classList.contains('visible') && mr_O.classList.contains('visible') && br_O.classList.contains('visible')) {
-      console.log('right column O wins!')   
+      // console.log('right column O wins!')   
+      winScreen()
     }
 
   // left diagonal
     if (tl_X.classList.contains('visible') && mm_X.classList.contains('visible') && br_X.classList.contains('visible')) {
       console.log('left diagonal X wins!')   
+      winScreen()
     } 
     if (tl_O.classList.contains('visible') && mm_O.classList.contains('visible') && br_O.classList.contains('visible')) {
-      console.log('left diagonal O wins!')   
+      // console.log('left diagonal O wins!')  
+      winScreen() 
     }
 
   // right diagonal
     if (tr_X.classList.contains('visible') && mm_X.classList.contains('visible') && bl_X.classList.contains('visible')) {
       console.log('right diagonal X wins!')   
+      winScreen()
     } 
     if (tr_O.classList.contains('visible') && mm_O.classList.contains('visible') && bl_O.classList.contains('visible')) {
-      console.log('right diagonal O wins!')   
+      // console.log('right diagonal O wins!')  
+      winScreen() 
     }
     
 }
 
+function winScreen() {
+  var container = document.getElementById('winnerContainer');
+  var winner = document.getElementById('winner');
 
+  container.style.display = 'block'
+
+  if (counter%2 == 0) {
+    winner.innerText = 'X Wins!'
+  } else {
+    winner.innerText = 'O Wins!'
+  }
+}
 // this event fires every time the panel is flipped. 
 // it first asks if the panel has already been flipped...
 // if the panel has already been flipped, then nothing happens. 'return' is a word that is supposed to say 'stop here and leave'. nothing happens after return
@@ -261,44 +288,46 @@ br.addEventListener('click', function() {
   })
 
 reset.addEventListener('click', function() {
-    tl.classList.remove('flipped')
-    tm.classList.remove('flipped')
-    tr.classList.remove('flipped')
-    ml.classList.remove('flipped')
-    mm.classList.remove('flipped')
-    mr.classList.remove('flipped')
-    bl.classList.remove('flipped')
-    bm.classList.remove('flipped')
-    br.classList.remove('flipped')
+  var Container = document.getElementById('winnerContainer');
+    Container.style.display = 'none'
+    tl.classList.remove('flipped');
+    tm.classList.remove('flipped');
+    tr.classList.remove('flipped');
+    ml.classList.remove('flipped');
+    mm.classList.remove('flipped');
+    mr.classList.remove('flipped');
+    bl.classList.remove('flipped');
+    bm.classList.remove('flipped');
+    br.classList.remove('flipped');
 
-    tl_X.classList.remove('visible', 'invisible')
-    tl_O.classList.remove('visible', 'invisible')
+    tl_X.classList.remove('visible', 'invisible');
+    tl_O.classList.remove('visible', 'invisible');
 
-    tm_X.classList.remove('visible', 'invisible')
-    tm_O.classList.remove('visible', 'invisible')
+    tm_X.classList.remove('visible', 'invisible');
+    tm_O.classList.remove('visible', 'invisible');
 
-    tr_X.classList.remove('visible', 'invisible')
-    tr_O.classList.remove('visible', 'invisible')
+    tr_X.classList.remove('visible', 'invisible');
+    tr_O.classList.remove('visible', 'invisible');
 
-    ml_X.classList.remove('visible', 'invisible')
-    ml_O.classList.remove('visible', 'invisible')
+    ml_X.classList.remove('visible', 'invisible');
+    ml_O.classList.remove('visible', 'invisible');
 
-    mm_X.classList.remove('visible', 'invisible')
-    mm_O.classList.remove('visible', 'invisible')
+    mm_X.classList.remove('visible', 'invisible');
+    mm_O.classList.remove('visible', 'invisible');
 
-    mr_X.classList.remove('visible', 'invisible')
-    mr_O.classList.remove('visible', 'invisible')
+    mr_X.classList.remove('visible', 'invisible');
+    mr_O.classList.remove('visible', 'invisible');
 
-    bl_X.classList.remove('visible', 'invisible')
-    bl_O.classList.remove('visible', 'invisible')
+    bl_X.classList.remove('visible', 'invisible');
+    bl_O.classList.remove('visible', 'invisible');
 
-    bm_X.classList.remove('visible', 'invisible')
-    bm_O.classList.remove('visible', 'invisible')
+    bm_X.classList.remove('visible', 'invisible');
+    bm_O.classList.remove('visible', 'invisible');
 
-    br_X.classList.remove('visible', 'invisible')
-    br_O.classList.remove('visible', 'invisible')
+    br_X.classList.remove('visible', 'invisible');
+    br_O.classList.remove('visible', 'invisible');
     
     counter = 0
 
-    console.log(counter)
+    console.log(counter);
 })
